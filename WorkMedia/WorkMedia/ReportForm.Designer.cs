f50be60a5719d196e30f5bce134cdda0b2aaf4db
Kodov <Kodov22@gmail.com>
2023-04-22 14:26:45 -0500

    complete - (start) report form

--- WorkMedia/WorkMedia/ReportForm.Designer.cs
+++ WorkMedia/WorkMedia/ReportForm.Designer.cs
@@ -31,6 +31,11 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label_TabHeader = new System.Windows.Forms.Label();
+            this.txtbox_report = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.lbl_charLimit = new System.Windows.Forms.Label();
+            this.txtbox_email = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -64,12 +69,72 @@
             this.label_TabHeader.Text = "Report";
             this.label_TabHeader.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // txtbox_report
+            // 
+            this.txtbox_report.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtbox_report.Location = new System.Drawing.Point(34, 214);
+            this.txtbox_report.Multiline = true;
+            this.txtbox_report.Name = "txtbox_report";
+            this.txtbox_report.Size = new System.Drawing.Size(225, 105);
+            this.txtbox_report.TabIndex = 1;
+            this.txtbox_report.TextChanged += new System.EventHandler(this.txtbox_report_TextChanged);
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.BackColor = System.Drawing.Color.Brown;
+            this.btn_submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_submit.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_submit.ForeColor = System.Drawing.Color.LightCoral;
+            this.btn_submit.Location = new System.Drawing.Point(87, 379);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(121, 42);
+            this.btn_submit.TabIndex = 2;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = false;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // lbl_charLimit
+            // 
+            this.lbl_charLimit.AutoSize = true;
+            this.lbl_charLimit.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_charLimit.ForeColor = System.Drawing.Color.Brown;
+            this.lbl_charLimit.Location = new System.Drawing.Point(31, 195);
+            this.lbl_charLimit.Name = "lbl_charLimit";
+            this.lbl_charLimit.Size = new System.Drawing.Size(60, 16);
+            this.lbl_charLimit.TabIndex = 3;
+            this.lbl_charLimit.Text = "char limit";
+            this.lbl_charLimit.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // txtbox_email
+            // 
+            this.txtbox_email.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtbox_email.Location = new System.Drawing.Point(34, 141);
+            this.txtbox_email.Name = "txtbox_email";
+            this.txtbox_email.Size = new System.Drawing.Size(225, 20);
+            this.txtbox_email.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(30, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email";
+            // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MistyRose;
             this.ClientSize = new System.Drawing.Size(304, 451);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbox_email);
+            this.Controls.Add(this.lbl_charLimit);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.txtbox_report);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -78,6 +143,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,5 +152,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label_TabHeader;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtbox_report;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label lbl_charLimit;
+        private System.Windows.Forms.TextBox txtbox_email;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WorkMedia/WorkMedia/ReportForm.cs
+++ WorkMedia/WorkMedia/ReportForm.cs
@@ -15,6 +15,22 @@ namespace WorkMedia
         public ReportForm()
         {
             InitializeComponent();
+            txtbox_report.MaxLength = 100;
+            lbl_charLimit.Text = txtbox_report.Text.Length + "/100";
+        }
+
+        private void txtbox_report_TextChanged(object sender, EventArgs e)
+        {
+            lbl_charLimit.Text = txtbox_report.Text.Length + "/100";
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            // TODO add db query
+
+            this.Close();
+
+            
         }
     }
 }
